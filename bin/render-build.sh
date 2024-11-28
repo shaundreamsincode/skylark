98a56853aff856f5514b271fe3021479a916a97f
Shaun Carland <shauncarland@Shauns-MacBook-Air.local>
2024-11-27 20:59:49 -0600

    Start building webpacker on deploy

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,8 +1,30 @@
 #!/usr/bin/env bash
-# exit on error
+
+
+#!/usr/bin/env bash
+# Exit on error
 set -o errexit
 
+# Install dependencies
 bundle install
+
+# Compile Webpacker assets
+bundle exec rails webpacker:compile
+
+# Precompile Rails assets (CSS, images, etc.)
 bundle exec rails assets:precompile
+
+# Clean up old assets
 bundle exec rails assets:clean
+
+# Migrate the database
 bundle exec rails db:migrate
+
+
+## exit on error
+#set -o errexit
+#
+#bundle install
+#bundle exec rails assets:precompile
+#bundle exec rails assets:clean
+#bundle exec rails db:migrate
