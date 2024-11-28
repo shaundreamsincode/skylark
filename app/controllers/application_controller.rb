5f3347a0dc6ed853702a1db4e66d05c577eda4b5
Shaun Carland <shauncarland@Shauns-MacBook-Air.local>
2024-11-28 08:46:04 -0600

    Fix layout of dashboard page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) if logged_in?
   end
 
   def track_page_view
